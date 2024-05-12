b370eb6616dd27614ac21eb91c6e6e6cdfdf38d1
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-05-12 11:37:02 +0900

    feat: implement clock

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 /**
  * @param {Object} props
  * @param {boolean} props.isHidden
  */
 const Clock = ({ isHidden }) => {
-  return <div hidden={isHidden}></div>;
+  const [date, setDate] = useState(new Date());
+
+  // useEffect(() => {
+  //   const newDate = new Date();
+  //   setDate(newDate);
+  // }, [setDate]);
+
+  setInterval(() => {
+    const newDate = new Date();
+    setDate(newDate);
+  }, 1000);
+
+  return (
+    <div hidden={isHidden}>
+      <h1>{`${String(date.getHours()).padStart(2, "0")}:${String(
+        date.getMinutes()
+      ).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}`}</h1>
+    </div>
+  );
 };
 
 export default Clock;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,12 @@
+import Clock from "../components/Clock";
+
 const Home = () => {
-  return <div>This is home</div>;
+  return (
+    <div>
+      This is home
+      <Clock isHidden={false} />
+    </div>
+  );
 };
 
 export default Home;

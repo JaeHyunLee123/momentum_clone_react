25ffe0bfcd07a13b1dbf56c99167e5f844e51418
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-05-19 15:30:52 +0900

    feat: implment converting seconds function

--- src/features/totalFocusTime/TotalFocusTime.jsx
+++ src/features/totalFocusTime/TotalFocusTime.jsx
@@ -1,12 +1,15 @@
 import { useSelector } from "react-redux";
+import { convertSeconds } from "../../libs/utils";
 
 const TotalFocusTime = () => {
   const focusTime = useSelector((state) => state.totalFocusTime.value);
 
+  const { hours, minutes, seconds } = convertSeconds(focusTime);
+
   return (
     <div>
-      <span>Today's focused time</span>
-      <span>{focusTime}</span>
+      <span> </span>
+      <span>{`Today's focused time: ${focusTime}`}</span>
     </div>
   );
 };

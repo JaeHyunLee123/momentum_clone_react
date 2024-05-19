5526dce1ff273717339614d6bbcf973b7d68402d
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-05-19 15:42:02 +0900

    feat: add style to totalFocustime viewer

--- src/features/totalFocusTime/TotalFocusTime.jsx
+++ src/features/totalFocusTime/TotalFocusTime.jsx
@@ -1,5 +1,32 @@
 import { useSelector } from "react-redux";
 import { convertSeconds } from "../../libs/utils";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  width: 300px;
+  height: 150px;
+
+  background-color: black;
+  opacity: 0.6;
+  border-radius: 20px;
+
+  margin: 10px 20px 10px 20px;
+
+  span {
+    color: whitesmoke;
+    font-size: 1.5rem;
+    text-align: center;
+    margin: 5px;
+  }
+
+  span: nth-child(2) {
+    font-size: 2rem;
+  }
+`;
 
 const TotalFocusTime = () => {
   const focusTime = useSelector((state) => state.totalFocusTime.value);
@@ -7,10 +34,13 @@ const TotalFocusTime = () => {
   const { hours, minutes, seconds } = convertSeconds(focusTime);
 
   return (
-    <div>
-      <span> </span>
-      <span>{`Today's focused time: ${focusTime}`}</span>
-    </div>
+    <Wrapper>
+      <span>Today's focused time</span>
+      <span>{`${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+        2,
+        "0"
+      )}:${String(seconds).padStart(2, "0")}`}</span>
+    </Wrapper>
   );
 };
 

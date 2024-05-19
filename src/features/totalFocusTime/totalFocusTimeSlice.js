24bcd08ada98419ba2b93f45921edf304e1ac499
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-05-19 15:58:49 +0900

    feature: initialize totalFocusTime to 0 when 6am is over

--- src/features/totalFocusTime/totalFocusTimeSlice.js
+++ src/features/totalFocusTime/totalFocusTimeSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const SLICE_NAME = "totalFocusTime";
+const LAST_RESET_TIME = "lastResetTime";
 
 /**
  *
@@ -11,21 +12,49 @@ const updateLocalStorage = (state) => {
   localStorage.setItem(SLICE_NAME, JSON.stringify(state.value));
 };
 
+/**
+ * Check if it's past 6 AM and reset the value if it is.
+ * @param {Object} state
+ * @param {number} state.value
+ */
+const checkAndResetAtSixAM = (state) => {
+  const now = new Date();
+  const lastResetTimeStr = localStorage.getItem(LAST_RESET_TIME);
+  const lastResetTime = lastResetTimeStr ? new Date(lastResetTimeStr) : null;
+
+  // If there's no record of the last reset time or it's a new day past 6 AM
+  if (
+    !lastResetTime ||
+    now.getDate() !== lastResetTime.getDate() ||
+    now.getHours() >= 6
+  ) {
+    state.value = 0;
+    localStorage.setItem(LAST_RESET_TIME, now.toISOString());
+  }
+};
+
 const localTotalFocusTimeStr = localStorage.getItem(SLICE_NAME);
 
 const initialState = localTotalFocusTimeStr
   ? { value: Number(JSON.parse(localTotalFocusTimeStr)) }
   : { value: 0 };
 
+// Check and reset if necessary when the application starts
+const preloadedState = { ...initialState };
+checkAndResetAtSixAM(preloadedState);
+
 export const totalFocusTimeSlice = createSlice({
   name: SLICE_NAME,
-  initialState,
+  initialState: preloadedState,
   reducers: {
     increment: (state) => {
+      // Ensure the state is checked and reset if needed before incrementing
+      checkAndResetAtSixAM(state);
       state.value += 1;
       updateLocalStorage(state);
     },
     initialize: (state) => {
+      // Explicitly set the value to 0 and update localStorage
       state.value = 0;
       updateLocalStorage(state);
     },

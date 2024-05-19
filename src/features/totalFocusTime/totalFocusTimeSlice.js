040049c8ca936b7ab91a869f434728780d3a1634
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-05-19 15:53:23 +0900

    feat: connect localStorage and totalFocusTime

--- src/features/totalFocusTime/totalFocusTimeSlice.js
+++ src/features/totalFocusTime/totalFocusTimeSlice.js
@@ -1,16 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const SLICE_NAME = "totalFocusTime";
+
+/**
+ *
+ * @param {Object} state
+ * @param {number} state.value
+ */
+const updateLocalStorage = (state) => {
+  localStorage.setItem(SLICE_NAME, JSON.stringify(state.value));
+};
+
+const localTotalFocusTimeStr = localStorage.getItem(SLICE_NAME);
+
+const initialState = localTotalFocusTimeStr
+  ? { value: Number(JSON.parse(localTotalFocusTimeStr)) }
+  : { value: 0 };
+
 export const totalFocusTimeSlice = createSlice({
-  name: "totalFocusTime",
-  initialState: {
-    value: 0,
-  },
+  name: SLICE_NAME,
+  initialState,
   reducers: {
     increment: (state) => {
       state.value += 1;
+      updateLocalStorage(state);
     },
     initialize: (state) => {
       state.value = 0;
+      updateLocalStorage(state);
     },
   },
 });
